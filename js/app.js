80a6e5662f9cf49953cbb24c668bb46c38679bba
Chris Reichert <chris.a.reichert@gmail.com>
2021-08-31 21:37:56 -0500

    added question constructor with current questions available

--- js/app.js
+++ js/app.js
@@ -2,3 +2,25 @@
 
 console.log('This is a test of the emergency boradcast system');
 
+let questionsArray = [];
+
+function questionsConstructor(category, question, answerCorrect, answerIncorrectA, answerIncorrectB, answerIncorrectC) {
+    this.category = category;
+    this.question = question;
+    this.answerCorrect = answerCorrect;
+    this.answerIncorrectA = answerIncorrectA;
+    this.answerIncorrectB = answerIncorrectB;
+    this.answerIncorrectC = answerIncorrectC;
+    questionsArray.push(this);
+}
+
+new questionsConstructor('Action', 'What does John McClane write on the first terrorist he kills in Die Hard?', 'HO-HO-HO', 'Now it\'s a party', 'Yippee Ki-Yay', 'Merry Christmas');
+new questionsConstructor('Comedy', 'What is the maximum setting for the amps for Spinal Tap?', '11', '10', 'Max', 'Loudest');
+new questionsConstructor('Comedy', 'In which Pennsylvania town does Bill Murray get stuck in Groundhog Day?', 'Punxsutawney', 'Pulaski', 'Pymatuning', 'Putnam');
+new questionsConstructor('SciFi', 'In which movie was the tag line "In space, no one can hear you scream."', 'Alien', 'Jason X', 'Gravity', 'Apollo 18');
+new questionsConstructor('Horror', 'Who was the killer in the first Friday the 13th movie?', 'Jason\'s Mom', 'Jason', 'Camp Counselor Steve', 'A Drifter');
+new questionsConstructor('Documentary', 'In Netflix\'s "Formula 1: Drive to Survive\" Series, which team won 4 Constructors Championships prior to the ascent of Lewis Hamilton and Mercedes?', 'Red Bull', 'McLaren', 'Ferrari', 'Haas');
+new questionsConstructor('Documentary', 'What did Carol Baskin cover her former husband with?', 'Sardine oil', 'Olive oil', 'Motor oil', 'Vegetable oil');
+
+
+console.log(questionsArray);
